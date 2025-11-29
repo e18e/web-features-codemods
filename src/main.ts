e75772e00993bbafbbf7a4fc4eb7e932d9410148
James Garbutt <43081j@users.noreply.github.com>
2025-11-29 10:00:01 +0000

    feat: add spread codemod (#6)
    Adds a codemod for the following:
    
    - `foo.concat(A, B)` -> `[...foo, ...A, ...B]`
    - `foo.concat([A, B])` -> `[...foo, ...[A, B]]`
    - `Object.assign({}, A, B)` -> `{...A, ...B}`
    - `fn.apply(null, args)` -> `fn(...args)`

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,5 @@ export {codemod as arrayToSpliced} from './codemods/array-to-spliced.js';
 export {codemod as exponentiation} from './codemods/exponentiation.js';
 export {codemod as nullishCoalescing} from './codemods/nullish-coalescing.js';
 export {codemod as postcssSignFunctions} from './codemods/postcss-sign-functions.js';
+export {codemod as spreadSyntax} from './codemods/spread-syntax.js';
 export {codemod as stringIncludes} from './codemods/string-includes.js';
